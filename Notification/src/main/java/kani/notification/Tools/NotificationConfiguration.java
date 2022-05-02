c6ef8fe6bf32ef0c5d808674c74e0e7a3071a16c
kaninda <73389238+kaninda@users.noreply.github.com>
2022-05-02 11:06:12 +0200

    Packing Jars to Docker images
    Packing Jars to Docker images
    
    Packing Jars to Docker images

--- Notification/src/main/java/kani/notification/Tools/NotificationConfiguration.java
+++ Notification/src/main/java/kani/notification/Tools/NotificationConfiguration.java
@@ -1,10 +1,28 @@
 package kani.notification.Tools;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class NotificationConfiguration {
 
+    @Value("${rabbitmq.queues.customer}")
+    private String customerQueue;
+
+    @Value("${rabbitmq.queues.fraud}")
+    private String fraudQueue;
+
+    @Bean
+    public Queue customerQueue() {
+        return new Queue(customerQueue);
+    }
+
+    @Bean
+    public Queue fraudQueue() {
+        return new Queue(fraudQueue);
+    }
 }
 
 
